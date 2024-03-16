bbee0e3b4996f5b35d5255dabed789101ed45f05
yogit <marketingconsultantsmx@gmail.com>
2024-03-15 17:32:31 -0700

    RRSS UPDATE

--- myenv/bin/activate.fish
+++ myenv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/julian/Team-Clayton/myenv"
+set -gx VIRTUAL_ENV "/var/www/html/Team-Clayton/myenv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
